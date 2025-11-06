388238b5c0ad84f718f0efce9fd332b68ef02768
Auro993 <sahooaurosmita204@gmail.com>
2025-11-07 00:35:34 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',   // ✅ HTML layout file
+  styleUrls: ['./app.component.css']     // ✅ Stylesheet file
+})
+export class AppComponent { }
